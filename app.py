6f8e9d044036d0961c3e55e40043e06905ad33a8
Charly Wargnier <cwar05@gmail.com>
2021-11-10 13:25:56 +0000

    Added icon

--- app.py
+++ app.py
@@ -26,8 +26,7 @@ def _max_width_():
         unsafe_allow_html=True,
     )
 
-
-st.set_page_config(page_title="📊 CSV Wrangler")
+st.set_page_config(page_icon=✂️, page_title="CSV Wrangler")
 
 # st.image("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/balloon_1f388.png", width=100)
 st.image(
